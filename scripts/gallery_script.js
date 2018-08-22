d25566185be1875e04dd0955db0a0d006dc58e4e
AjithPG <ajithpg008@gmail.com>
2018-08-23 02:27:01 +0530

    Added Gallery Page

---
+++ scripts/gallery_script.js
@@ -0,0 +1,14 @@
+(function () {
+	'use strict';
+	var dialog = document.querySelector('dialog');
+	var showModalButton = document.querySelector('.show-modal');
+	if (!dialog.showModal) {
+		dialogPolyfill.registerDialog(dialog);
+	}
+	showModalButton.addEventListener('click', function () {
+		dialog.showModal();
+	});
+	dialog.querySelector('.close').addEventListener('click', function () {
+		dialog.close();
+	});
+}());
